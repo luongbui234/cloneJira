701c716371f6dff31704509ef3cc57d8ae9b6df3
luongbui234 <luongbui0610@gamil.com>
2025-04-16 18:59:28 +0700

    da tao branch projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,24 @@
+"use client";
 import React from "react";
 
+import { Pagination } from "flowbite-react";
+import { useState } from "react";
+
 export default function ProjectsPage() {
-  return <div>ProjectsPage</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const onPageChange = (page: number) => setCurrentPage(page);
+  return (
+    <div className="flex overflow-x-auto sm:justify-center">
+      <Pagination
+        layout="pagination"
+        currentPage={currentPage}
+        totalPages={1000}
+        onPageChange={onPageChange}
+        previousLabel="Go back"
+        nextLabel="Go forward"
+        showIcons
+      />
+    </div>
+  );
 }
