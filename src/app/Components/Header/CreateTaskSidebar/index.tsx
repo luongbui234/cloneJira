7ab417e47c982a459cf067117055dee1b22ae69d
luongbui234 <luongbui0610@gamil.com>
2025-05-20 00:23:20 +0700

    func createTask

--- src/app/Components/Header/CreateTaskSidebar/index.tsx
+++ src/app/Components/Header/CreateTaskSidebar/index.tsx
@@ -1,21 +1,134 @@
-import { Button, Drawer, Form, Input, InputNumber, Select, Slider } from "antd";
-import React from "react";
+import { createTaskService } from "@/app/services/taskService";
+import { RootState } from "@/app/store/store";
+import { CreateTask } from "@/app/types/task";
+import {
+  BugOutlined,
+  FileOutlined,
+  MenuOutlined,
+  MinusOutlined,
+  PauseOutlined,
+  UpOutlined,
+} from "@ant-design/icons";
+import { Avatar, Button, Drawer, Form, Input, Select, Slider } from "antd";
+import React, { useMemo } from "react";
+import toast from "react-hot-toast";
 import { FaFileMedical } from "react-icons/fa6";
+import { useSelector } from "react-redux";
 const { TextArea } = Input;
 
 interface Props {
   isCreateTaskOpen: boolean;
   handleCreateTaskClose: () => void;
+  valueTotalEstimate: number;
+  handleChangeTotalEstimate: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  valueTimeSpent: number;
+  handleChangeTimeSpent: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSetValueTimeSpent: (value: number) => void;
 }
 
 export default function CreateTaskSidebarComponent({
   isCreateTaskOpen,
   handleCreateTaskClose,
+  valueTotalEstimate,
+  handleChangeTotalEstimate,
+  valueTimeSpent,
+  handleChangeTimeSpent,
+  handleSetValueTimeSpent,
 }: Props) {
-  const onFinish = (values: string) => {
-    console.log("Success:", values);
+  const [form] = Form.useForm();
+
+  const { listProject: projects } = useSelector((state: RootState) => {
+    return state.project;
+  });
+  const {
+    typeTask: types,
+    statusTask: status,
+    priorityTask: priorities,
+  } = useSelector((state: RootState) => {
+    return state.task;
+  });
+  const { listUserNotYetAdded: users } = useSelector((state: RootState) => {
+    return state.project;
+  });
+
+  const listProject = projects.map((project, index) => {
+    return { key: index, label: project.projectName, value: project.id };
+  });
+  const listType = types.map((type, index) => {
+    return {
+      key: index,
+      label: (
+        <div className="flex space-x-1">
+          {type.id === 1 ? (
+            <BugOutlined className="text-xl text-red-500" />
+          ) : (
+            <FileOutlined className="text-xl text-green-500" />
+          )}
+          <p>{type.taskType}</p>
+        </div>
+      ),
+      value: type.id,
+    };
+  });
+  const listStatus = status.map((status, index) => {
+    return { key: index, label: status.statusName, value: status.statusId };
+  });
+  const listPriority = priorities.map((priority, index) => {
+    const iconPriority = [
+      { key: 0, label: <UpOutlined className="text-red-500" /> },
+      { key: 1, label: <MenuOutlined className="text-yellow-300" /> },
+      {
+        key: 3,
+        label: <PauseOutlined rotate={90} className="text-blue-500" />,
+      },
+      { key: 4, label: <MinusOutlined className="text-green-400" /> },
+    ];
+    return {
+      key: index,
+      label: (
+        <div className="space-x-2">
+          {iconPriority[index].label}
+          <span>{priority.priority}</span>
+        </div>
+      ),
+      value: priority.priorityId,
+    };
+  });
+  const listUser = users.map((user, index) => {
+    return {
+      key: index,
+      label: (
+        <div className="flex items-center space-x-1">
+          <Avatar size={"small"} src={user.avatar} />
+          <span>{user.name}</span>
+        </div>
+      ),
+      value: user.userId,
+    };
+  });
+
+  const handleCreateTask = async (data: CreateTask) => {
+    const newData = { ...data };
+    newData.description = newData.description || "";
+    newData.listUserAsign = newData.listUserAsign || [];
+    console.log("newData: ", newData);
+
+    const actionCreateTask = await createTaskService(newData);
+    if (actionCreateTask.statusCode === 200) {
+      form.resetFields();
+      handleSetValueTimeSpent(0);
+      toast.success("Create task successfully");
+    } else {
+      toast.error(actionCreateTask.message);
+    }
   };
 
+  useMemo(() => {
+    form.setFieldValue(
+      "timeTrackingRemaining",
+      valueTotalEstimate - valueTimeSpent
+    );
+  }, [form, valueTotalEstimate, valueTimeSpent]);
   return (
     <Drawer
       title={
@@ -31,175 +144,116 @@ export default function CreateTaskSidebarComponent({
       footer={
         <div className="flex justify-end space-x-3">
           <Button onClick={handleCreateTaskClose}>Cancel</Button>
-          <Button onClick={handleCreateTaskClose} type="primary">
+          <Button onClick={() => form.submit()} type="primary">
             Create
           </Button>
         </div>
       }
     >
       <Form
+        form={form}
         name="basic"
         layout="vertical"
-        onFinish={onFinish}
+        onFinish={handleCreateTask}
         className="w-full"
+        initialValues={{
+          originalEstimate: 0,
+          timeTrackingSpent: 0,
+          timeTrackingRemaining: 0,
+        }}
       >
         <Form.Item
           label="Project"
-          name="project"
+          name="projectId"
           rules={[{ required: true, message: "Project name is required" }]}
         >
           <Select
-            placeholder={"Select a project category"}
-            options={[
-              {
-                label: "Dự án web",
-                value: 1,
-              },
-              {
-                label: "Dự án phần mềm",
-                value: 2,
-              },
-              {
-                label: "Dự án di động",
-                value: 3,
-              },
-            ]}
+            showSearch
+            optionFilterProp="label"
+            placeholder={"Select a project"}
+            options={listProject}
           />
         </Form.Item>
 
         <Form.Item
           label="Task name"
           name="taskName"
-          rules={[{ required: true, message: "Project name is required" }]}
+          rules={[{ required: true, message: "Task name is required" }]}
         >
           <Input />
         </Form.Item>
 
         <Form.Item
           label="Status"
-          name="status"
-          rules={[{ required: true, message: "Project category is required" }]}
+          name="statusId"
+          rules={[{ required: true, message: "Status is required" }]}
         >
-          <Select
-            placeholder={"Select a project category"}
-            options={[
-              {
-                label: "Dự án web",
-                value: 1,
-              },
-              {
-                label: "Dự án phần mềm",
-                value: 2,
-              },
-              {
-                label: "Dự án di động",
-                value: 3,
-              },
-            ]}
-          />
+          <Select placeholder={"Select a status"} options={listStatus} />
         </Form.Item>
 
         <Form.Item
           label="Priority"
-          name="priority"
-          rules={[{ required: true, message: "Project category is required" }]}
+          name="priorityId"
+          rules={[{ required: true, message: "Priority is required" }]}
         >
-          <Select
-            placeholder={"Select a project category"}
-            options={[
-              {
-                label: "Dự án web",
-                value: 1,
-              },
-              {
-                label: "Dự án phần mềm",
-                value: 2,
-              },
-              {
-                label: "Dự án di động",
-                value: 3,
-              },
-            ]}
-          />
+          <Select placeholder={"Select a priority"} options={listPriority} />
         </Form.Item>
 
         <Form.Item
           label="Task type"
-          name="taskType"
-          rules={[{ required: true, message: "Project category is required" }]}
+          name="typeId"
+          rules={[{ required: true, message: "Type is required" }]}
         >
+          <Select placeholder={"Select a type"} options={listType} />
+        </Form.Item>
+
+        <Form.Item label="Assigners" name="listUserAsign">
           <Select
-            placeholder={"Select a project category"}
-            options={[
-              {
-                label: "Dự án web",
-                value: 1,
-              },
-              {
-                label: "Dự án phần mềm",
-                value: 2,
-              },
-              {
-                label: "Dự án di động",
-                value: 3,
-              },
-            ]}
+            placeholder={"Choose assignees..."}
+            mode="multiple"
+            options={listUser}
           />
         </Form.Item>
 
         <Form.Item
-          label="Assigners"
-          name="assigners"
-          rules={[{ required: true, message: "Project category is required" }]}
+          label="Total estimate"
+          name="originalEstimate"
+          rules={[
+            { required: true, message: "Total estimated hours is required" },
+          ]}
         >
-          <Select
-            placeholder={"Select a project category"}
-            options={[
-              {
-                label: "Dự án web",
-                value: 1,
-              },
-              {
-                label: "Dự án phần mềm",
-                value: 2,
-              },
-              {
-                label: "Dự án di động",
-                value: 3,
-              },
-            ]}
+          <Input
+            type="number"
+            onChange={handleChangeTotalEstimate}
+            min={0}
+            style={{ width: "120px" }}
+            suffix={"m"}
           />
         </Form.Item>
 
         <p>Time Tracking</p>
         <div className="flex justify-between">
           <Form.Item
-            label="Total estimated hours"
-            name="totalEstimatedHours"
-            rules={[
-              { required: true, message: "Project category is required" },
-            ]}
+            label="Time spent"
+            name="timeTrackingSpent"
+            rules={[{ required: true, message: "Time spent is required" }]}
           >
-            <InputNumber min={0} defaultValue={0} style={{ width: "120px" }} />
+            <Input
+              type="number"
+              onChange={handleChangeTimeSpent}
+              min={0}
+              max={valueTotalEstimate}
+              style={{ width: "120px" }}
+            />
           </Form.Item>
 
-          <Form.Item
-            label="Hours spent"
-            name="hoursSpent"
-            rules={[
-              { required: true, message: "Project category is required" },
-            ]}
-          >
-            <InputNumber min={0} defaultValue={0} style={{ width: "120px" }} />
+          <Form.Item label="Time remaining" name="timeTrackingRemaining">
+            <Input type="number" style={{ width: "120px" }} disabled />
           </Form.Item>
         </div>
-        <Slider defaultValue={30} />
-        <div className="flex justify-between">
-          <span>6 hour(s) spent</span>
-          <span>3 hour(s) remaining</span>
-        </div>
+        <Slider min={0} value={valueTimeSpent} max={valueTotalEstimate} />
 
-        <Form.Item label="Descriptions">
+        <Form.Item label="Descriptions" name="description">
           <TextArea rows={4} />
         </Form.Item>
       </Form>
--- src/app/Components/Header/index.tsx
+++ src/app/Components/Header/index.tsx
@@ -7,20 +7,34 @@ import MenuSideBarComponent from "./MenuSidebar";
 import CreateTaskSidebarComponent from "./CreateTaskSidebar";
 import AvatarProfileComponent from "./AvatarProfile";
 import MenuIconComponent from "./MenuIcon";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/app/store/store";
 import { Button } from "antd";
+import {
+  getTaskPriorityService,
+  getTaskStatusService,
+  getTaskTypeService,
+} from "@/app/services/taskService";
+import { setPriorityTask, setStatusTask, setTypeTask } from "@/redux/taskSlice";
+import { getUserService } from "@/app/services/userService";
+import { setListProject, setListUserNotYetAdded } from "@/redux/projectSlice";
+import { getProjectService } from "@/app/services/projectService";
 
 export default function HeaderComponents() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const [isCreateTaskOpen, setIsCreateTaskOpen] = useState(false);
 
+  const [valueTotalEstimate, setValueTotalEstimate] = useState(0);
+  const [valueTimeSpent, setValueTimeSpent] = useState(0);
+
   const pathName = usePathname();
 
   const router = useRouter();
 
   const [isClient, setIsClient] = useState(false);
+
+  const dispatch = useDispatch();
 
   const { me } = useSelector((state: RootState) => {
     return state.me;
@@ -46,11 +60,39 @@ export default function HeaderComponents() {
     setIsCreateTaskOpen(false);
   };
 
+  const handleChangeTotalEstimate = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setValueTotalEstimate(Number(e.target.value));
+  };
+  const handleChangeTimeSpent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValueTimeSpent(Number(e.target.value));
+  };
+
+  const handleSetValueTimeSpent = (value: number) => {
+    setValueTimeSpent(value);
+  };
+
   const handleSignout = () => {
     router.push("/signin");
     window.location.href = "/signin";
     localStorage.removeItem("ME");
   };
+
+  useEffect(() => {
+    (async () => {
+      const actionTaskType = await getTaskTypeService();
+      dispatch(setTypeTask(actionTaskType.content));
+      const actionTaskStatus = await getTaskStatusService();
+      dispatch(setStatusTask(actionTaskStatus.content));
+      const actionPriority = await getTaskPriorityService();
+      dispatch(setPriorityTask(actionPriority.content));
+      const actionUser = await getUserService(undefined);
+      dispatch(setListUserNotYetAdded(actionUser.content));
+      const actionProject = await getProjectService(undefined);
+      dispatch(setListProject(actionProject.content));
+    })();
+  }, []);
 
   return isClient && me ? (
     <nav className="p-3 bg-orange-300 flex justify-between">
@@ -100,6 +142,11 @@ export default function HeaderComponents() {
       <CreateTaskSidebarComponent
         isCreateTaskOpen={isCreateTaskOpen}
         handleCreateTaskClose={handleCreateTaskClose}
+        valueTotalEstimate={valueTotalEstimate}
+        handleChangeTotalEstimate={handleChangeTotalEstimate}
+        valueTimeSpent={valueTimeSpent}
+        handleChangeTimeSpent={handleChangeTimeSpent}
+        handleSetValueTimeSpent={handleSetValueTimeSpent}
       />
     </nav>
   ) : null;
